572ff952ae87895f502dda9f94f8f1b0075c44cf
loveliness <lovelinessmoe@gmail.com>
2021-03-25 16:49:14 +0800

    New

---
+++ src/test/java/top/icu/study_manager/StudyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.icu.study_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
